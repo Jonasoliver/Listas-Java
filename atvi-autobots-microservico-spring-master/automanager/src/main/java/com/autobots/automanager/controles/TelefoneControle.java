7f37baa051b9f5d40cb3300e8e5f34f8b43efb97
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-10-24 07:42:42 -0300

    Feat: atualizei o telefone controle

--- atvi-autobots-microservico-spring-master/automanager/src/main/java/com/autobots/automanager/controles/TelefoneControle.java
+++ atvi-autobots-microservico-spring-master/automanager/src/main/java/com/autobots/automanager/controles/TelefoneControle.java
@@ -1,42 +1,47 @@
 package com.autobots.automanager.controles;
 
-
 import com.autobots.automanager.entidades.Telefone;
-import com.autobots.automanager.repositorios.TelefoneRepositorio;
+import com.autobots.automanager.entidades.Cliente;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/telefone")
+@RequestMapping("/cliente/{clienteId}/telefone")
 public class TelefoneControle {
     @Autowired
-    private TelefoneRepositorio repositorio;
+    private ClienteRepositorio repositorio;
 
-    @GetMapping("/{id}")
-    public Telefone obterTelefone(@PathVariable Long id) {
-        return repositorio.findById(id).orElse(null);
+    @PostMapping("/adicionar")
+    public void adicionarTelefone(@PathVariable long clienteId, @RequestBody Telefone telefone) {
+        Cliente cliente = repositorio.findById(clienteId).orElse(null);
+        if (cliente != null) {
+            cliente.getTelefones().add(telefone);
+            repositorio.save(cliente);
+        }
     }
 
-    @GetMapping
-    public List<Telefone> obterTelefones() {
-        return repositorio.findAll();
+    @PutMapping("/atualizar/{telefoneId}")
+    public void atualizarTelefone(@PathVariable long clienteId, @PathVariable long telefoneId, @RequestBody Telefone telefoneAtualizado) {
+        Cliente cliente = repositorio.findById(clienteId).orElse(null);
+        if (cliente != null) {
+            for (Telefone telefone : cliente.getTelefones()) {
+                if (telefone.getId().equals(telefoneId)) {
+                    telefone.setDdd(telefoneAtualizado.getDdd());
+                    telefone.setNumero(telefoneAtualizado.getNumero());
+                    break;
+                }
+            }
+            repositorio.save(cliente);
+        }
     }
 
-    @PostMapping
-    public void adicionarTelefone(@RequestBody Telefone telefone) {
-        repositorio.save(telefone);
-    }
-
-    @PutMapping
-    public void atualizarTelefone(@RequestBody Telefone telefone) {
-        repositorio.save(telefone);
-
-    }
-
-    @DeleteMapping("/{id}")
-    public void removerTelefone(@PathVariable Long id) {
-        repositorio.deleteById(id);
+    @DeleteMapping("/deletar/{telefoneId}")
+    public void deletarTelefone(@PathVariable long clienteId, @PathVariable long telefoneId) {
+        Cliente cliente = repositorio.findById(clienteId).orElse(null);
+        if (cliente != null) {
+            cliente.getTelefones().removeIf(telefone -> telefone.getId().equals(telefoneId));
+            repositorio.save(cliente);
+        }
     }
 }
